266d5f81a36ba5db31132e19aac5125e9af473de
“aswinireddy1234” <chegireddyaswinireddy@gmail>
2023-03-12 19:05:15 +0530

    Added: Ability to Create Contacts

---
+++ UC1-CreateContacts.java
@@ -0,0 +1,20 @@
+package com.addressbooksystem;
+import java.util.ArrayList;
+
+public class AddressBookSystem {
+    private static final ArrayList<ArrayList <String>> address_book = new ArrayList<>();
+
+    public void createContacts(ArrayList <String> contact){
+
+        AddressBook.address_book.add(contact);
+
+        for (ArrayList <String> i: AddressBook.address_book ){
+            for (String j: i){
+                System.out.println(j);
+            }
+        }
+    }
+    public static void main(String []args) {
+        System.out.println("Welcome to Address Book Program!");
+    }
+}
